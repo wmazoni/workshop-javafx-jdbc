9141167e81541e48e438a222252ef150651329e3
Wellington Andrade <wmazoni@protonmail.com>
2020-05-07 21:29:05 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -136,6 +138,25 @@ public class SellerFormController implements Initializable {
 		}
 		obj.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
+
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -159,7 +180,7 @@ public class SellerFormController implements Initializable {
 		Constraints.setTextFieldDouble(txtBaseSalary);
 		Constraints.setTextFieldMaxLength(txtEmail, 60);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
-		
+
 		initializeComboBoxDepartment();
 	}
 
@@ -177,9 +198,8 @@ public class SellerFormController implements Initializable {
 		}
 		if (entity.getDepartment() == null) {
 			comboBoxDepartment.getSelectionModel().selectFirst();
-		}
-		else {
-			comboBoxDepartment.setValue(entity.getDepartment());	
+		} else {
+			comboBoxDepartment.setValue(entity.getDepartment());
 		}
 	}
 
@@ -194,10 +214,11 @@ public class SellerFormController implements Initializable {
 
 	private void setErrorMessages(Map<String, String> error) {
 		Set<String> fields = error.keySet();
-
-		if (fields.contains("name")) {
-			labelErrorName.setText(error.get("name"));
-		}
+		
+		labelErrorName.setText((fields.contains("name") ? error.get("name") : ""));
+		labelErrorEmail.setText((fields.contains("email") ? error.get("email") : ""));
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? error.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? error.get("baseSalary") : ""));
 
 	}
 
